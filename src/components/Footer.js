320d7f80f4ee85316807c58d9703934924ffba46
chriss daito <s21910284@student.unklab.ac.id>
2021-12-01 17:57:28 +0800

    Final Exam Weather

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <footer className="py-5 bg-white">
+      <div className="container">
+        <p className="m-0 text-center text-black">Copyright &copy; Weather App 2021</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+
+const About = () => {
+  return(
+    <>
+      <Navigation />
+
+      <div className="container my-5">
+        <h1>About this Application</h1>
+        <p>Prodi : Informatika</p>
+        <p>Nama : Pala'langan, Radocen Chrisnov</p>
+        <p>Kelas : Front-end Web Development / Web Programming B</p>
+        <p>REG : S21910284</p>
+        <p>NIM : 105021910035</p>
+      </div>
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+      <Footer />
+    </>
+  );
+}
+
+export default About;
